b6fb6751de6bddf8b887ce628a2edb0571a7d43e
Rose Soriano <rosesoriano.dtx@gmail.com>
2022-01-12 22:38:51 -0800

    edited packery options

--- js/initPackery.js
+++ js/initPackery.js
@@ -6,6 +6,7 @@ imagesLoaded( grid, function() {
   // init Packery after all images have loaded
   pckry = new Packery( grid, {
     itemSelector: '.grid-item',
-    percentPosition: true
+    percentPosition: true,
+    gutter: 10
   });
 });
